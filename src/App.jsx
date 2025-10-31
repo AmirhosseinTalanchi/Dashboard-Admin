e5b5da767465ba81a4fe382d9662100a754c1052
AmirhosseinTalanchi <www.amirhosseintalanchi.84@gmail.com>
2025-10-31 19:59:49 +0330

    creat project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import routes from './routes'
+import { useRoutes } from 'react-router-dom'
+
+function App() {
+
+  let router = useRoutes(routes)
+
+  return (
+    <>
+      {router}
+    </>
+  )
+}
+
+export default App
---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+
+
+let routes = [
+    {path:'/', element: <Home></Home>},
+    {path:'/Projects', element: <Projects></Projects>},
+
+
+]
+
+export default routes
